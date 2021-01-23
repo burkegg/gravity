c396be7edd6b8635d22bc3360b1b5a619311a2e2
Burke Green <burke@orionlabs.io>
2021-01-22 21:27:43 -0800

    Calc webworker does not work reverting to play w/ original class

--- src/calc.worker.js
+++ src/calc.worker.js
@@ -1,19 +1,107 @@
 // eslint-disable-next-line no-restricted-globals
 self.addEventListener("message", startCounter);
+let Vector = require('victor');
+
 
 function startCounter(event) {
   // eslint-disable-next-line no-restricted-globals
   let startTime = performance.now();
-  let start = 0
-  let initial = event.data;
-  let data = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-  let init = 0
-  while (start < 1000000) {
-    start += 1
-    initial += 1
+  let sizeTimestep = 0.00002
+  let G = 400
+  let initDataNeedsVectors = event.data[0]
+  let dataToWrite = []
+
+  let initData = initDataNeedsVectors.map(ballData => {
+    return {pos: new Vector(ballData.pos.x, ballData.pos.y), vel: new Vector(ballData.vel.x, ballData.vel.y), color: ballData.color, mass: ballData.mass, ballIdx: ballData.ballIdx}
+  })
+
+  console.log('initData in worker', initData)
+  const calcOneForce = (ballData1, ballData2) => {
+
+
+    // given which ball (e.g. ball1, ball2....) by index (0, 1, 2...)
+    // so we can get their mass and maybe look up other properties later if we need.
+    let s2 = ballData1.pos.distanceSq(ballData2.pos)
+
+    let f = (G * ballData1.mass * ballData2.mass) / (s2 + 0.2 * s2)
+    if (s2 < 5) {
+      f = 0
+    }
+    let b2Copy = {}
+    b2Copy.pos = new Vector(0,0)
+    b2Copy.pos.copy(ballData2.pos)
+    let fVector = b2Copy.pos.subtract(ballData1.pos)
+    fVector.normalize()
+    fVector.multiplyScalar(f)
+    return fVector
   }
-  let endTime = performance.now();
-  var timeDiff = endTime - startTime;
-  console.log(timeDiff + " ms");
-  this.postMessage(timeDiff)
+
+  const applyForce = (ballData) => {
+    // RE-think
+    let f = calcNetForce(ballData.ballIdx)
+    let massVect = new Vector(ballData.mass, ballData.mass)
+    f.divide(massVect)
+
+    f.multiplyScalar(sizeTimestep)
+    // create new vel and pos vectors and push them into history
+    let newVel  = new Vector(ballData.vel.x, ballData.vel.y)
+    let newPos = new Vector(ballData.pos.x, ballData.pos.y)
+    /*
+    a = vf - vo / t
+    vf = at + vo
+     */
+
+    newVel.add(f)
+
+    let deltaPos = new Vector(newVel.x, newVel.y)
+    deltaPos.multiplyScalar(sizeTimestep)
+    /*
+    v = xf - xo / t
+    xf = vt + xo
+     */
+
+    newPos.add(deltaPos)
+    let data = {pos: newPos, vel: newVel, mass: ballData.mass, color: ballData.color, ballIdx: ballData.ballIdx}
+    return data
+  }
+
+  const calcNetForce = (idx) => {
+    // add a row to the history
+    // let info = initData
+    let netForce = new Vector(0,0)
+
+    initData.forEach((ballData, dataIdx) => {
+      if (idx !== dataIdx) {
+        let f2Add = calcOneForce(initData[idx], initData[dataIdx])
+        netForce.add(f2Add)
+      }
+    })
+    return netForce
+  }
+
+
+
+
+  if (event.data[0].length) {
+    for (let step = 0; step < 10000; step++) {
+      let tempData = []
+      initData.forEach((ball, ballNum) => {
+        let tempOneBallData = applyForce(ball)
+        tempData[ballNum] = tempOneBallData
+      })
+      dataToWrite = tempData
+    }
+
+    let endTime = performance.now();
+    var timeDiff = endTime - startTime;
+    console.log(timeDiff + " ms");
+    this.postMessage(dataToWrite)
+  }
+
+
+
+
+
+
+
 }
